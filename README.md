cca05b395e4b9ec327b808526d1445c2b7aad867
Jonathan Peterson <me@eonasdan.com>
2022-01-26 17:11:15 -0500

    fix script update readme

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-Adapated and improved from https://chrislayers.com/2021/08/01/scripting-winget/
+#What is this?
+
+When you have a fresh Windows 10/11 environment it can be annoying to install every SINGLE piece of software you need. 
+
+This powershell script will download the `install` and `uninstall` text files in this repo and run `winget` on each line of the install file and attempt to uninstall any Windows Store apps listed in the uninstall file.
+
+Elevated permissions are required to perform the uninstall.
+
+You are free to fork this repo and modify the in/uninstall files to your hearts content.
 
 
 
 
-Run:
+Run this from a powershell window
 ```bh
-#Run this from a powershell window
-iex "& { $(irm https://raw.githubusercontent.com/Eonasdan/desktop-automation/main/automate.ps1) } -url https://raw.githubusercontent.com/Eonasdan/desktop-automation/main -install"
+iex "& { $(irm https://raw.githubusercontent.com/Eonasdan/desktop-automation/main/automate.ps1) } -url https://raw.githubusercontent.com/Eonasdan/desktop-automation/main -install -uninstall"
 ```
+
+
+
+Adapted and improved from https://chrislayers.com/2021/08/01/scripting-winget/
--- automate.ps1
+++ automate.ps1
@@ -4,9 +4,24 @@ param (
     [switch] $uninstall
 )
 
-Add-Type -AssemblyName System.Net.Http
-#Clear-Host
+#Install WinGet
+#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith('msixbundle') } | Select-Object -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+
+Add-Type -AssemblyName System.Net.Http
 $httpClient = New-Object System.Net.Http.HttpClient
 
 function Get-Stream{
@@ -15,7 +30,6 @@ function Get-Stream{
 		[string] $fetch
 	)
 
-    Write-Host "Downloading install file"    
     $response = $httpClient.GetAsync($fetch)
     $response.Wait()
 
@@ -28,6 +42,10 @@ if ($install) {
     $installStream = (Get-Stream "$url/install.txt")
     while (-not $installStream.EndOfStream){
         $app = $installStream.ReadLine()
+        if ($app -eq "404: Not Found") {
+            Write-Host "Installation file not found"
+            break
+        }
         Write-host "Trying:" $app
         $listApp = winget list --exact -q $app
         if (![String]::Join("", $listApp).Contains($app)) {
@@ -43,22 +61,26 @@ if ($install) {
 #Remove Apps
 
 if ($uninstall) {
-    function Test-Administrator  
-    {  
+    function Test-Administrator
+    {
         $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-        (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+        (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
     }
 
     if (Test-Administrator) {
 
     Write-Output "Removing Apps"
 
-    $uninstallStream = (Get-Stream "$url/uninstall.txt")
-    while (-not $uninstallStream.EndOfStream){
-        $app = $uninstallStream.ReadLine()
-        Write-host "Uninstalling:" $app
-        Get-AppxPackage -allusers $app | Remove-AppxPackage
-    }
+        $uninstallStream = (Get-Stream "$url/uninstall.txt")
+        while (-not $uninstallStream.EndOfStream){
+            $app = $uninstallStream.ReadLine()
+            if ($app -eq "404: Not Found") {
+                Write-Host "Uninstall file not found"
+                break
+            }
+            Write-host "Uninstalling:" $app
+            Get-AppxPackage -allusers $app | Remove-AppxPackage
+        }
     }
     else {
         Write-Output "Uninstall requires elevated permissions. Run the script as an admin"
